4ddc2174a0c5e1f23b286a97a464aef567ed7acd
Johnny Hsu <johnnyhsu1106@gmail.com>
2016-11-12 16:37:36 -0700

    Add files via upload

--- DealerFactory.java
+++ DealerFactory.java
@@ -2,7 +2,7 @@ public class DealerFactory extends PokerGameFactory{
 	public Dealer createDealer(){
 		return new Dealer();
 	}
-	public Dealer createDealer(String name, int tableNum, Deck deck){
-		return Dealer(name, tableNum, deck);
+	public Dealer createDealer(String name){
+		return Dealer(name);
 	}
 }
