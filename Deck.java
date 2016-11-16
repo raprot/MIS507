5742e23b790369a5300141584562221c394502a5
itdoor <joshuaz@email.arizona.edu>
2016-11-15 18:09:02 -0700

    Add files via upload

--- Deck.java
+++ Deck.java
@@ -1,24 +1,24 @@
+import java.util.ArrayList;
+
 public class Deck{
-   private static Card[] newDeck = new Card[52];
-   private final int DECK_SIZE = 52;
+   private static Card[] newDeck = new Card[52];//Array of Card objects that
+                                                //holds the Deck of Cards.
    
-   private static int c = 0,
-                  topCard = 52;
-   
-   
-   //Card[] newDeck = new Card[DECK_SIZE];
-    
- // private static int[] suitInt = {1,2,3,4};
-  
- // private static int[] cardInt = {14,2,3,4,5,6,7,8,9,10,11,12,13};
-  
+   private final int DECK_SIZE = 52; //Final int variable to capture how many
+                                     //indices a Deck array should have.
+   private       int topCard = 52;   //Integer variabe to track the top card
+                                     //in a Deck Card array as cards are taken
+                                     //from the deck;
+                  
   public static  String[] suitName = {"Spades", "Hearts", "Diamonds", 
-                                      "Clubs"};
+                                      "Clubs"};//Default Deck Suits. Public
+                                               //so that it can be overwritten.
   
-  public static  String[] rankName = 
-                                       {"Ace", "Two", "Three", "Four", "Five",
+  public static  String[] rankName = {"Ace", "Two", "Three", "Four", "Five",
                                        "Six", "Seven", "Eight", "Nine",
                                        "Ten", "Jack", "Queen", "King"};
+                                      //Default Deck Ranks. Public so that it
+                                      //can be overwritten.
                         
  public Deck(){
     
@@ -26,17 +26,20 @@ public class Deck{
       
       
       for(int r = 0; r < rankName.length; r++){
+        int rVal = 0,
+            sVal = 4 - s;
         
+        if(r == 0){rVal = 14;}else{rVal = r + 1;}
         
-      this.newDeck[c] = new Card(rankName[r], suitName[s], (s + 1) , (r + 1));
-      
-      //
-       
-       this.c++;
+      this.newDeck[((rankName.length * s) + r)] = 
+           new Card(suitName[s], rankName[r], sVal , rVal);
       }
   }
     
- }
+ }//Deck class default, and only, constructor. Loops through 2 Arrays. One is 
+  //an array of suit names Spades, Hearts etc. The other is an array of face
+  //values, Ace, Two etc. Uses the Array indices for each array to set 
+  //the suitNumber and rankNumber.
  
  public Card getCard(){
   this.topCard = this.topCard - 1;
@@ -47,10 +50,12 @@ public class Deck{
     return this.newDeck[this.topCard];}
   else{
     return this.newDeck[this.topCard];}
-}
+}//Pulls the top card from the deck. Uses the private int topCard to keep track
+ //of which card in the array is the top card. Returns the card at the array
+ //index taht matches the topCard value and decrements topCard.
+ 
  
  public void shuffleDeck(){
-   //reference: https://www.dotnetperls.com/fisher-yates-shuffle
    for(int i = 0; i < this.newDeck.length; i++){
      int shuf = i + (int)(Math.random() * (DECK_SIZE - i));
      
@@ -58,14 +63,47 @@ public class Deck{
      this.newDeck[shuf] = this.newDeck[i];
      this.newDeck[i] = shuffleCard;     
    }
-   
-  // for(int i = 0; i < this.newDeck.length; i++){
-       //System.out.println(newDeck[i].getSuit() + " " + newDeck[i].getRank() + " " + newDeck[i].getSuitNumber()  + " " + newDeck[i].getRankNumber());
-     //}
+ }//Uses the Fisher-Yates shuffle method to randomly shuffle the deck. 
+  //Exact method used can be found here:
+  //reference: https://www.dotnetperls.com/fisher-yates-shuffle
+  //This is a somewhat straightforward and simple shuffle method so it 
+  //does not stray far from the outline given by the source material.
+ 
+  
+ public int getNbrCardsInDeck(){
+   return topCard + 1;
+ }//Getter to return the number of cards left in the deck.
+ 
+ public Card[] getDeck(){
+   return newDeck;
+ }//Getter to return the Card[] a Deck type object holds.
+ 
+
+ public String toString(){
+  String returnString = "";
+  for(int i = 0; i < this.newDeck.length; i++){
+      
+    returnString += (newDeck[i].getSuit() + " " + 
+                                  newDeck[i].getRank() + " " + 
+                                  newDeck[i].getSuitNumber() + " " + 
+                                  newDeck[i].getRankNumber() + "\n");
+     }
+  
+  return returnString;
+ }//AF implementation for the Deck Class.
+ 
+ public boolean RepOk(Deck testDeck){
+   boolean testRepOk = true;
+  /* for(int i = 0; i < this.testDeck.getDeck().length; i++){
+     testRepOK = (this.testDeck.getDeck()[i].RepOK && testDeck[i] != null);
+       if(!testRepOk){break;}
+    }*/
+   return testRepOk;
  }
-   
-  /*
+  
+/*  
   public static void main(String args[]){
+    Card testCard;
     Card myCard, myCard2;
     
     Deck pokerDeck = new Deck();
@@ -85,9 +123,13 @@ public class Deck{
     for(int i = 53; i > 0; i--){
       pokerDeck.getCard();
     }
+        
+    System.out.println(newDeck[51].RepOK(newDeck[51]));
+    
+    System.out.println(pokerDeck.toString());
     
     System.out.println(newDeck[51].toString());
     
-  }*/
-  
+  }
+  */
 }
