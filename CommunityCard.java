6cb9ed38a1440a7b55c616be84493890eac62e19
MichelleZ22 <mvziegler22@email.arizona.edu>
2016-11-16 21:03:22 -0700

    Add files via upload

--- CommunityCard.java
+++ CommunityCard.java
@@ -3,14 +3,11 @@ import java.util.ArrayList;
 class CommunityCard{
   private ArrayList<Card> hand = new ArrayList<>();
   
-  public void getCommunityCards(Card card){
-    hand.add(card);
-  }
-  public void setCards(){
-    
+  public void setCommunityCard(Card card){
+    this.hand.add(card);
   }
   
-  public ArrayList<Card> getCommunityHand(){
+  public ArrayList<Card> getCommunityCards(){
     return hand;
   }
   
