b9b4cd174c9ececd0778a6d5f316885053d614e3
itdoor <joshuaz@email.arizona.edu>
2016-11-18 17:59:16 -0700

    Add files via upload

--- PokerHand.java
+++ PokerHand.java
@@ -1,20 +1,35 @@
+
 import java.util.*;
 
 class PokerHand implements Comparable<PokerHand>{
+  private HandStrengthAnalyzer hsa = new HandStrengthAnalyzer();
   private Card[] hand = new Card[5];
   private static  ArrayList<Card> temp = new ArrayList<>();
+  private int handStrength;
   
-  public int compareTo(PokerHand hand){
+  @Override public int compareTo(PokerHand pokerHand){
     return 0;
   }
   
-  public static void getBestHand(ArrayList<Card> playerCard, ArrayList<Card> communityCard){
+  private int getHandStrength(){
+    handStrength = this.hsa.getHandStrengthVal();
+    return handStrength;
+  }
+  
+  private Card[] getHand(){
+    return hand;
+  }
+    
+  public Card[] getBestHand(ArrayList<Card> playerCard, ArrayList<Card> communityCard){
     for(int i = 0; i < playerCard.size(); i++){
       temp.add(playerCard.get(i));
     }
     for(int i = 0; i < communityCard.size(); i++){
       temp.add(communityCard.get(i));
     }
+    
+    hand =  hsa.getBestHand(temp);
+    return hand;
   }
   
   //Unecessary Code
@@ -24,17 +39,18 @@ class PokerHand implements Comparable<PokerHand>{
 
   
   public static void main(String[] args){
+    Card [] catcher;
     Deck d = new Deck();
     d.shuffleDeck();
     PlayerHand p = new PlayerHand();
-    p.getPlayerCard(d.getCard());
-    p.getPlayerCard(d.getCard());
+    p.setPlayerHand(d.getCard());
+    p.setPlayerHand(d.getCard());
     CommunityCard c = new CommunityCard();
-    c.getCommunityCards(d.getCard());
-    c.getCommunityCards(d.getCard());
-    c.getCommunityCards(d.getCard());
-    c.getCommunityCards(d.getCard());
-    c.getCommunityCards(d.getCard());
+    c.setCommunityCard(d.getCard());
+    c.setCommunityCard(d.getCard());
+    c.setCommunityCard(d.getCard());
+    c.setCommunityCard(d.getCard());
+    c.setCommunityCard(d.getCard());
     
     PokerHand pk = new PokerHand();
         
@@ -46,13 +62,22 @@ class PokerHand implements Comparable<PokerHand>{
       System.out.println(c.getCommunityHand().get(i).toString());
     }*/
     
-    pk.getBestHand(p.getPlayerHand(), c.getCommunityHand());
-    //pk.compareTo(pk.getTemp());
-
-    for(int i = 0; i < pk.getTemp().size(); i++){
+    catcher = pk.getBestHand(p.getPlayerHand(), c.getCommunityCards());
+    
+    
+     for(int i = 0; i < pk.getTemp().size(); i++){
       System.out.println(pk.getTemp().get(i).toString());
     }
+     
     
+    
+    System.out.println("\n\n\n\n\n");
+    for(int i = 0; i < catcher.length; i++){
+      System.out.println(catcher[i]);
+    }
+    
+    //pk.compareTo(pk.getTemp());
+ 
     
     
   }
